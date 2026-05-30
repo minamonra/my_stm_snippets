03af9fe9c72aca2f5036bbb67a09bd388db84333
Comandante <minamonra@gmail.com>
2026-05-30 22:49:00 +0300

    Update common.h

--- F4/F412-blink/inc/common.h
+++ F4/F412-blink/inc/common.h
@@ -15,7 +15,7 @@ void delay_nop(uint32_t ticks);
 void blink_led(uint16_t freq);
 void morse_send_nb(const char* str);
 
-// Единственная точка входа для инициализации всего железа
+// Инициализации всего
 void System_Init(void);
 
 #endif // COMMON_H
