97e62b8df3616234688ed439666b199b7eb0676b
Comandante <minamonra@gmail.com>
2026-05-30 20:36:53 +0300

    Update pindefs.h

--- F4/F412-blink/inc/pindefs.h
+++ F4/F412-blink/inc/pindefs.h
@@ -12,10 +12,4 @@
 #define LED_SYSTEM_OFF      GPIOB->BSRR = GPIO_BSRR_BS_2                        // Установка PB2 в 1 (погасить)
 #define LED_SYSTEM_TOGGLE   GPIOB->ODR  ^= GPIO_ODR_OD2                         // Инверсия состояния PB2
 
-// -----------------------------------------------------------------------------
-// Внешнее реле (Пин PC13)
-// -----------------------------------------------------------------------------
-#define RELAY_ON            GPIOC->BSRR = GPIO_BSRR_BS_13                       // Установка PC13 в 1 (включить)
-#define RELAY_OFF           GPIOC->BSRR = GPIO_BSRR_BR_13                       // Сброс PC13 в 0 (выключить)
-
 #endif // PINDEFS_H
